35396f5f1afbda4eb0c68ded8e6d77c3ab2be7fa
grey4uk <grey4uk14@gmail.com>
2020-03-12 22:19:08 +0200

    Updates

---
+++ precache-manifest.3707e8c2fc14b841c2333967d7ffecb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b5074659519d914ed6923a0d130d04b",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "3619843ca040ff11600b",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.ae511a6a.chunk.css"
+  },
+  {
+    "revision": "35497c729fc7ce91f49f",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.cc9cb97c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.cc9cb97c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3619843ca040ff11600b",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.dd588e30.chunk.js"
+  },
+  {
+    "revision": "29041d2fed37885842fe",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.d31d0001.js"
+  }
+]);
